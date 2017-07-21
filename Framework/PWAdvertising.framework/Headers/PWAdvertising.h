627c0ab0aefb6efc31d123223685c2edcb188017
Rongbo Yang <ryang@phunware.com>
2017-07-21 16:07:59 -0500

    Update beta

---
+++ Framework/PWAdvertising.framework/Headers/PWAdvertising.h
@@ -0,0 +1,33 @@
+//
+//  PWAdvertising.h
+//  PWAdvertising
+//
+//  Created by Rongbo Yang on 7/19/17.
+//  Copyright © 2017 Phunware, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PWAdvertising.
+FOUNDATION_EXPORT double PWAdvertisingVersionNumber;
+
+//! Project version string for PWAdvertising.
+FOUNDATION_EXPORT const unsigned char PWAdvertisingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PWAdvertising/PublicHeader.h>
+
+#import "PWAds.h"
+#import "PWAdsAppTracker.h"
+#import "PWAdsBannerView.h"
+#import "PWAdsBrowserControllerDelegate.h"
+#import "PWAdsConstants.h"
+#import "PWAdsInterstitial.h"
+#import "PWAdsLandingPage.h"
+#import "PWAdsNativeAd.h"
+#import "PWAdsNativeAdController.h"
+#import "PWAdsNativeAdLoader.h"
+#import "PWAdsNativeAdManager.h"
+#import "PWAdsNativeAdView.h"
+#import "PWAdsRequest.h"
+#import "PWAdsRewardedVideo.h"
+#import "PWAdsVideoInterstitial.h"
--- PWAdvertisingBeta.podspec
+++ PWAdvertisingBeta.podspec
@@ -5,18 +5,18 @@ Pod::Spec.new do |s|
   s.summary  = 'Phunware PWAdvertising SDK (BETA)'
   s.homepage = 'https://github.com/phunware/beta-ios'
   s.authors  = { 'Phunware' => 'info@phunware.com' }
-	s.source   = { :git => "https://github.com/phunware/beta-ios.git", :tag => "PWAdvertising-3.6.2.9" }
+	s.source   = { :git => "https://github.com/phunware/beta-ios.git", :tag => "PWAdvertising-3.6.9" }
   s.requires_arc = true
 
   s.resource  = 'Bundle/PWAds.bundle'
   s.source_files = 'Framework/**/*.{h,m}'
   s.public_header_files = 'Framework/PWAdvertising.framework/Versions/A/Headers/*.h'
   s.ios.vendored_frameworks = 'Framework/PWAdvertising.framework'
-  
+
   s.ios.deployment_target = '9.0'
 
   s.dependency 'PWCore'
-  
+
   s.ios.frameworks = 'Security', 'QuartzCore', 'SystemConfiguration', 'MobileCoreServices', 'CoreTelephony', 'MessageUI', 'EventKit', 'EventKitUI', 'CoreMedia', 'AVFoundation', 'MediaPlayer', 'AudioToolbox', 'AdSupport', 'StoreKit', 'WebKit'
   s.library        = 'sqlite3', 'z'
 
