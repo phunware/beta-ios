da00db5069fb7676fa845fde65c476a3d03a8f22
Jenkins <hostmaster@phunware.com>
2017-08-16 07:58:14 -0700

    Update beta

--- Framework/PWEngagement.framework/Versions/A/Headers/PWEngagement.h
+++ Framework/PWEngagement.framework/Versions/A/Headers/PWEngagement.h
@@ -14,7 +14,7 @@
 #import <PWEngagement/PWMELocalNotification.h>
 #import <PWEngagement/PWMEAttributeManager.h>
 
-static NSString *const PWEngagementVersion = @"3.1.1.4";
+static NSString *const PWEngagementVersion = @"3.1.1.5";
 
 /**
  The message identifier key which may be included in the notification's userInfo dictionary.
