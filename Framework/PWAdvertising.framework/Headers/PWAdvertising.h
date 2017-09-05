bf49daaf778adb91a96431f72933fba16d3471ec
Jenkins <hostmaster@phunware.com>
2017-09-05 07:49:53 -0700

    Update beta

--- Framework/PWAdvertising.framework/Headers/PWAdvertising.h
+++ Framework/PWAdvertising.framework/Headers/PWAdvertising.h
@@ -21,7 +21,7 @@
 #import "PWAdsVideoBase.h"
 #import <PWCore/PWCore.h>
 
-static NSString * const PWAdvertisingVersion = @"3.6.3.9";
+static NSString * const PWAdvertisingVersion = @"3.6.3.10";
 
 @interface PWAdvertising : NSObject
 
