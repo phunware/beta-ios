66d3f8ace2d58d1fc4788221b4a8492bc518b6f2
Jenkins <hostmaster@phunware.com>
2018-04-25 08:25:53 -0700

    Update beta

--- Framework/PWCore.framework/Headers/PWCore.h
+++ Framework/PWCore.framework/Headers/PWCore.h
@@ -16,7 +16,7 @@
 #import <PWCore/PWBundleManager.h>
 #import <PWCore/PWAPIManager.h>
 
-static NSString * const PWCoreVersion = @"3.4.0.2";
+static NSString * const PWCoreVersion = @"3.4.0.3";
 
 /**
  `PWCore` implements core functionality used in all MaaS modules. All MaaS modules have a `PWCore` dependency.
