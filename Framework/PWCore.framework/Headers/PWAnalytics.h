598eb8fb827b9b88e1b6e9b7e20d488a4aaf8bf8
Jenkins <hostmaster@phunware.com>
2018-05-10 07:23:57 -0700

    Update beta

--- Framework/PWCore.framework/Headers/PWAnalytics.h
+++ Framework/PWCore.framework/Headers/PWAnalytics.h
@@ -24,106 +24,37 @@
 + (void)addEvent:(NSString *__nonnull)eventName;
 
 /**
- Add an event to the analytics queue.
- @param eventName Name of the event. 
- @param eventNamespace The namespace of the analytic event. Used for custom event processing.
- */
-+ (void)addEvent:(NSString *__nonnull)eventName eventNamespace:(NSString *__nonnull)eventNamespace;
-
-/**
  Add an event to the analytics queue. Parameters must follow these rules:
-  (1) Keys and values must be `NSString` objects.
-  (2) There may be no more than 10 parameters. Extra parameters will be ignored.
-  (3) Each key or value that is longer than 256 characters will be truncated.
- @param eventName Name of the event. 
- @param parameters `NSString` key / value parameters. For example, `@{@"key" : @"value"}`. 
- */
-+ (void)addEvent:(NSString *__nonnull)eventName withParameters:(NSDictionary *__nullable)parameters;
-
-/**
- Add an event to the analytics queue. Parameters must follow these rules:
-  (1) Keys and values must be `NSString` objects.
-  (2) There may be no more than 10 parameters. Extra parameters will be ignored.
-  (3) Each key or value that is longer than 256 characters will be truncated.
- @param eventName Name of the event. 
- @param parameters `NSString` key / value parameters. For example, `@{@"key" : @"value"}`. 
- @param eventNamespace The namespace of the analytic event. Used for custom event processing.
- */
-+ (void)addEvent:(NSString *__nonnull)eventName withParameters:(NSDictionary *__nullable)parameters eventNamespace:(NSString *__nullable)eventNamespace;
-
-/**
- Start a timed event. Calling `startTimedEvent:` with the same `eventName` prior to `endTimedEvent:` will overwrite the previous start time.
- @param eventName Name of the event to start.
- */
-+ (void)startTimedEvent:(NSString *__nonnull)eventName;
-
-/**
- Start a timed event. Calling `startTimedEvent:` with the same `eventName` prior to `endTimedEvent:` will overwrite the previous start time.
- @param eventName Name of the event to start.
- @param eventNamespace The namespace of the analytic event. Used for custom event processing.
- */
-+ (void)startTimedEvent:(NSString *__nonnull)eventName eventNamespace:(NSString *__nullable)eventNamespace;
-
-/**
- Start a timed event with the specified parameters. Calling `startTimedEvent:` with the same `eventName` prior to `endTimedEvent:` will overwrite the previous start time. Parameters must follow these rules:
-  (1) Keys and values must be `NSString` objects.
-  (2) There may be no more than 10 parameters. Extra parameters will be ignored.
-  (3) Each key or value that is longer than 256 characters will be truncated.
- @param eventName Name of the event to start. 
- @param parameters `NSString` key / value parameters. For example, `@{@"key" : @"value"}`. 
- */
-+ (void)startTimedEvent:(NSString *__nonnull)eventName withParameters:(NSDictionary *__nullable)parameters;
-
-/**
- Start a timed event with the specified parameters. Calling `startTimedEvent:` with the same `eventName` prior to `endTimedEvent:` will overwrite the previous start time. Parameters must follow these rules:
-  (1) Keys and values must be `NSString` objects.
-  (2) There may be no more than 10 parameters. Extra parameters will be ignored.
-  (3) Each key or value that is longer than 256 characters will be truncated.
- @param eventName Name of the event to start.
+ (1) Keys must be NSString objects and values must be either NSString or NSNumber objects.
+ (2) There may be no more than 25 parameters. Extra parameters will be ignored.
+ (3) Each key or value that is longer than 256 characters will be truncated.
+ @param eventName Name of the event.
  @param parameters `NSString` key / value parameters. For example, `@{@"key" : @"value"}`.
- @param eventNamespace The namespace of the analytic event. Used for custom event processing.
  */
-+ (void)startTimedEvent:(NSString *__nonnull)eventName withParameters:(NSDictionary *__nullable)parameters eventNamespace:(NSString *__nullable)eventNamespace;
++ (void)addEvent:(NSString *__nonnull)eventName withParameters:(NSDictionary<NSString *, id> *__nullable)parameters;
 
 /**
- Pause a timed event with the specified `eventName`. If an event is ended while still in a paused state, then the paused time will be used as the end time. If an event is already paused, this method will do nothing.
- @param eventName The name of the event to pause. 
+ Set a screen name for automatic screen view events.
+ @param screenName Screen name for the view event.
  */
-+ (void)pauseTimedEvent:(NSString *__nonnull)eventName;
++ (void)setScreenName:(NSString *__nonnull)screenName;
 
 /**
- Resume a timed event with the specified `eventName`. If an event is ended while still in a paused state, then the paused time will be used as the end time. If an event is already paused, this method will do nothing. 
- @param eventName The name of the event to resume. 
+ Set an app section for automatic screen view events.
+ @param appSection App section for the view event.
  */
-+ (void)resumeTimedEvent:(NSString *__nonnull)eventName;
++ (void)setAppSection:(NSString *__nullable)appSection;
 
 /**
- End a timed event with the specified `eventName`. The event will be ignored if the event duration is less than one second. This method will do nothing if the event name cannot be found.
- @param eventName The name of the event to end. 
+ Set global parameters for all events.
+ @param parameters Parameters included for all events until cleared.
  */
-+ (void)endTimedEvent:(NSString *__nonnull)eventName;
++ (void)setGlobalParameters:(NSDictionary *__nullable)parameters;
 
 /**
- End a timed event with the specified `eventName`. The event will be ignored if the event duration is less than one second. This method will do nothing if the event name cannot be found. Parameters must follow these rules:
-  (1) Keys and values must be `NSString` objects.
-  (2) There may be no more than 10 parameters. Extra parameters will be ignored.
-  (3) Each key or value that is longer than 256 characters will be truncated.
- @param eventName The name of the event to end. 
- @param parameters `NSString` key / value parameters. For example, `@{@"key" : @"value"}`. 
+ Determines whether automatic screen views are captured.
+ @param enable Will automatically capture screen view events or not. Defaults to enabled.
  */
-+ (void)endTimedEvent:(NSString *__nonnull)eventName withParameters:(NSDictionary *__nullable)parameters;
-
-/**
- End a timed event with the specified `eventName`. The event will be ignored if the event duration is less than one second. This method will do nothing if the event name cannot be found. Parameters must follow these rules:
-  (1) Keys and values must be `NSString` objects.
-  (2) There may be no more than 10 parameters. Extra parameters will be ignored.
-  (3) Each key or value that is longer than 256 characters will be truncated.
- @param eventName The name of the event to end.
- @param parameters `NSString` key / value parameters. For example, `@{@"key" : @"value"}`.
- @param eventNamespace The namespace of the analytic event. Used for custom event processing.
- */
-+ (void)endTimedEvent:(NSString *__nonnull)eventName withParameters:(NSDictionary *__nullable)parameters eventNamespace:(NSString *__nullable)eventNamespace;
-
-
++ (void)enableAutomaticScreenViewEvents:(BOOL)enable;
 
 @end
