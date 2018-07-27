8ddc8852bd3a1b84cb0fbc8a2256d16564e668d6
jenkins <hostmaster@phunware.com>
2018-07-27 01:26:46 -0700

    Update beta

--- Framework/PWCore.framework/Headers/PWCore.h
+++ Framework/PWCore.framework/Headers/PWCore.h
@@ -20,7 +20,7 @@
 #import <PWCore/PWCoreDevice.h>
 #import <PWCore/PWFeatureFlagger.h>
 
-static NSString * const PWCoreVersion = @"3.6.0.50";
+static NSString * const PWCoreVersion = @"3.6.0.51";
 
 /**
  `PWCore` implements core functionality used in all MaaS modules. All MaaS modules have a `PWCore` dependency.
