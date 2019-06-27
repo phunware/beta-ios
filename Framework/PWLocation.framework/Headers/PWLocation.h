ddf593fd5203690823b4566c185e0630b0daf3fc
jenkins <hostmaster@phunware.com>
2019-06-27 12:13:54 -0700

    Update beta

--- Framework/PWLocation.framework/Headers/PWLocation.h
+++ Framework/PWLocation.framework/Headers/PWLocation.h
@@ -2,7 +2,40 @@
 //  PWLocation.h
 //  PWLocation
 //
-//  Copyright © 2019 Phunware Inc. All rights reserved.
+//  Copyright (c) 2014 Phunware Inc. All rights reserved.
 //
 
-#import <PWLocation/PWManagedPositionAlgorithmWrapper.h>
+#import <Foundation/Foundation.h>
+
+#import <PWLocation/PWLocationProtocol.h>
+#import <PWLocation/PWIndoorLocation.h>
+#import <PWLocation/PWLocationManager.h>
+#import <PWLocation/PWMSELocationManager.h>
+#import <PWLocation/PWSLLocationManager.h>
+#import <PWLocation/PWVBLocationManager.h>
+#import <PWLocation/PWManagedLocationManager.h>
+#import <PWLocation/PWManagedProviderInfo.h>
+#import <PWLocation/PWGPSLocationManager.h>
+#import <PWLocation/PWMockLocationManager.h>
+#import <PWLocation/PWMockLocationManagerConfiguration.h>
+#import <PWLocation/PWMockLocationStep.h>
+
+/**
+ PWLocation is a location provider SDK. This SDK encompasses a variety of indoor location providers and can be used in conjunction with PWMapKit to delivery and display indoor location location updates.
+ */
+
+static NSString * const PWLocationVersion = @"4.0.0.16";
+static NSString * const PWLocationAlgoVersion = @"Drop-for-SDK-Team-062619";
+
+@interface PWLocation : NSObject
+
+///----------------------
+/// @name Utility Methods
+///----------------------
+
+/**
+ Returns the name of the SDK, `PWLocation`.
+ */
++ (NSString *)serviceName;
+
+@end
