2e029c9bdeec32fe91ab4bbb986ab3ef3141a2e8
jenkins <hostmaster@phunware.com>
2019-10-23 10:32:40 -0700

    Update beta

--- Framework/PWMapKit.framework/Headers/PWMapKit.h
+++ Framework/PWMapKit.framework/Headers/PWMapKit.h
@@ -28,7 +28,7 @@
 #import <PWMapKit/PWSharedLocation.h>
 #import <PWMapKit/PWLocationSharingDelegate.h>
 
-static NSString *const PWMapKitVersion = @"4.0.0.26";
+static NSString *const PWMapKitVersion = @"4.0.0.27";
 
 @interface PWMapKit : NSObject
 
