a98b8e5398514d2f2d0109a3fe50ecbf1d0cf832
jenkins <hostmaster@phunware.com>
2020-03-02 14:10:33 -0800

    Update beta

--- Framework/PWLocation.framework/Headers/PWLocation.h
+++ Framework/PWLocation.framework/Headers/PWLocation.h
@@ -24,7 +24,7 @@
  PWLocation is a location provider SDK. This SDK encompasses a variety of indoor location providers and can be used in conjunction with PWMapKit to delivery and display indoor location location updates.
  */
 
-static NSString * const PWLocationVersion = @"4.0.0.48";
+static NSString * const PWLocationVersion = @"4.0.0.49";
 static NSString * const PWLocationAlgoVersion = @"2.3";
 
 @interface PWLocation : NSObject
